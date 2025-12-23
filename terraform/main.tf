dade3f6eaa9d999dc47dbf579597496aea2da404
aaren <aarenkanapathy@gmail.com>
2025-12-24 06:13:04 +0800

    5th Commit region problem changed AMI insertion from manual to auto find and apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,15 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu Creators)
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 # 1. THE FIREWALL (Open Port 5000)
 resource "aws_security_group" "sg" {
   name = "allow_flask"
@@ -29,7 +38,7 @@ resource "aws_security_group" "sg" {
 
 # 2. THE SERVER (Ubuntu + Docker Script)
 resource "aws_instance" "server" {
-  ami                    = "ami-0c7217cdde317cfec" # Ubuntu 22.04
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
