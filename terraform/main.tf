1bc382e1e4b837d4d2387d775b636e9c854b30d3
aaren <aarenkanapathy@gmail.com>
2025-12-24 06:54:38 +0800

    8th commit to fix race condition docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,8 +60,8 @@ resource "aws_instance" "server" {
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y docker.io
-              sudo systemctl enable docker
               sudo systemctl start docker
+              sleep 30
               sudo docker run -d -p 5000:5000 aaren17/devops-demo:latest
               EOF
 }
