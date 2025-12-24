35f944447f827d0cd368cd3b8d2fa681f8d9f872
aaren <aarenkanapathy@gmail.com>
2025-12-24 12:07:58 +0800

    fixed syntax error

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,34 +10,59 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Dynamic AMI Lookup (Using the code you provided)
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical
+  owners      = ["099720109477"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
-# Firewall Rules
 resource "aws_security_group" "sg" {
-  name = "allow_app_traffic"
+  name = "allow_flask"
 
-  ingress { from_port = 22; to_port = 22; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }     # SSH
-  ingress { from_port = 5000; to_port = 5000; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] } # Flask
-  ingress { from_port = 3000; to_port = 3000; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] } # Grafana
-  ingress { from_port = 9090; to_port = 9090; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] } # Prometheus
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-# The EC2 Instance
 resource "aws_instance" "server" {
-  ami                    = data.aws_ami.ubuntu.id # Uses the dynamic ID from the data block
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name               = "my-key" # Ensure this matches your AWS console key name
+  key_name               = "my-key" # Ensure this matches your imported AWS key name
 
   user_data = <<-EOF
               #!/bin/bash
@@ -48,8 +73,7 @@ resource "aws_instance" "server" {
               EOF
 }
 
-# Elastic IP Association
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.server.id
-  allocation_id = "eipalloc-0faf631d22521d0ab" # Fixed Elastic IP ID
+  allocation_id = "eipalloc-0faf631d22521d0ab" # From your image_3a45fd.png
 }
