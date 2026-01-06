835ab3505d5aed217b464b4a7527913da6a1a240
aaren <aarenkanapathy@gmail.com>
2026-01-06 12:34:46 +0800

    Testing terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "server" {
   key_name               = "my-key"
 
   tags = {
-    Name = "DevOps-Server-v3"
+    Name = "DevOps-Server-v4"
   }
   
   # Corrected User Data to install Docker + Docker Compose Plugin
